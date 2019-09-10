730d7bb5f3a0fbbde8936d080a9460b780e8bc01
jinhaiyue <jinhaiyue@rocoinfo.com>
2019-09-10 17:47:46 +0800

    vue.config.js配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+const path = require('path')
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
 let proxyDev = {
   '/api/*': {
     target: 'http://localhost:9091'
@@ -15,3 +19,28 @@ try {
   argv = process.argv
 }
 let proxy = argv[1] === 'mock' ? mockProxy : proxyDev
+
+module.exports = {
+  publicPath: process.env.PUBLIC_URL,
+  assetsDir: process.env.ASSETS_URL,
+  pages: {
+    index: {
+      entry: 'src/entries/main.js'
+    },
+    login: {
+      entry: 'src/entries/login.js'
+    }
+  },
+  devServer: {
+    proxy
+  },
+  configureWebpack: config => {
+    config.resolve = {
+      extensions: ['.js', '.vue', '.json', '.css'],
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js',
+        '@': resolve('src')
+      }
+    }
+  }
+}
