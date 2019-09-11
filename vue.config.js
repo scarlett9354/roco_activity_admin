6f2be11a7e54691017a76dd7da931eddda39aa24
jinhaiyue <jinhaiyue@rocoinfo.com>
2019-09-11 10:39:16 +0800

    根目录配置文件

--- vue.config.js
+++ vue.config.js
@@ -23,14 +23,14 @@ let proxy = argv[1] === 'mock' ? mockProxy : proxyDev
 module.exports = {
   publicPath: process.env.PUBLIC_URL,
   assetsDir: process.env.ASSETS_URL,
-  pages: {
-    index: {
-      entry: 'src/entries/main.js'
-    },
-    login: {
-      entry: 'src/entries/login.js'
-    }
-  },
+  // pages: {
+  //   index: {
+  //     entry: 'src/entries/main.js'
+  //   },
+  //   login: {
+  //     entry: 'src/entries/login.js'
+  //   }
+  // },
   devServer: {
     proxy
   },
